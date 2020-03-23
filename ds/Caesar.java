c2d9a8b3bd5df4af742844f7e93fd39a0429146e
drenu83 <45490901+drenu83@users.noreply.github.com>
2020-03-23 22:18:54 +0100

    Update Caesar.java

--- ds/Caesar.java
+++ ds/Caesar.java
@@ -1,3 +1,8 @@
 public class Caesar{
     public static final String alfabeti = "abcdefghijklmnopqrstuvwyxz" ; 
+    public static String encoding (String plaintext, int key)
+    {
+        plaintext = plaintext.toLowerCase() ;
+        String ciphertext = "" ;
+    }
     }
