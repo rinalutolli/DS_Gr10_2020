76d18582349ac9584a55437b94b13223aff30ee0
drenu83 <45490901+drenu83@users.noreply.github.com>
2020-03-25 00:02:57 +0100

    Create RailFence.java

---
+++ ds/RailFence.java
@@ -0,0 +1,7 @@
+public class RailFence {
+
+	public static void main(String[] args) {
+		if("encrypt".equals(args[0]))
+		       System.out.println(encoding(args[1],Integer.parseInt(args[2])));
+       
+	}
